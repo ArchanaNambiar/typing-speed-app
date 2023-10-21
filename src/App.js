6cd6f8951c87c638e86e73df849443139ccd5279
ArchanaNambiar <archanacnambiar@gmail.com>
2023-10-21 23:16:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import Header from './components/Header';
-import Paragraph from './components/Paragraph';
-import ResetButton from './components/ResetButton';
-import Timer from './components/Timer';
-import AccountButton from './components/AccountButton';
-import SocialMedia from './components/SocialMedia';
-import BackgroundDropdown from './components/BackgroundDropdown';
+import { GlobalStyle } from "./Styles/global";
+import { ThemeProvider } from "styled-components";
+import { useTheme } from "./Context/ThemeContext";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import UserPage from "./Pages/UserPage";
+
 
 function App() {
+
+  const {theme} = useTheme()
   return (
-    <div className="app">
-      <Header />
-      <div className="content">
-        <div className="left">
-          <Paragraph />
-          <ResetButton />
-          <Timer />
-        </div>
-        <div className="right">
-          <AccountButton />
-          <SocialMedia />
-          <BackgroundDropdown />
-        </div>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ToastContainer/>
+      <GlobalStyle />
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/user" element={<UserPage/>}></Route>
+      </Routes>
+
+    </ThemeProvider>
   );
 }
 
